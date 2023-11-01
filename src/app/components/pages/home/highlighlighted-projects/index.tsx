276680e6d70cdaffbabc029947b9e61f4d43239b
Paulo Ziembick <pauloziembick@gmail.com>
2023-11-01 16:35:22 -0300

    cube

--- src/app/components/pages/home/highlighlighted-projects/index.tsx
+++ src/app/components/pages/home/highlighlighted-projects/index.tsx
@@ -14,15 +14,36 @@ export const HighLightedProjects = () => {
         <ProjectCard />
         <HorizontalDivider />
         <ProjectCard />
-        <HorizontalDivider />   
+        <HorizontalDivider />
 
-        <p>
-          <span>Se interessou?</span>
-          <Link href="/projects" className="inline-flex">
-            Ver todos
-            <HiArrowNarrowRight size={18}/>
-          </Link>
-        </p>     
+        <div>
+          <h4>Todos os Projetos</h4>
+          <div>
+            <button>Projetos</button>
+            <div className="cube-container">
+              <div className="cube">
+                <div className="face front">
+                  <img src="/images/html-cube.svg" alt="Cube Html" />
+                </div>
+                <div className="face back">
+                  <img src="/images/css.svg" alt="Cube Css" />
+                </div>
+                <div className="face right">
+                  <img src="/images/javascript.svg" alt="Cube JS" />
+                </div>
+                <div className="face left">
+                  <img src="/images/react.svg" alt="Cube React" />
+                </div>
+                <div className="face top">
+                  <img src="/images/typescript.svg" alt="Cube React" />
+                </div>
+                <div className="face bottom">
+                  <img src="/images/next-js.svg" alt="Cube React" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
